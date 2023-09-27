bc1a73fae8f3df3b8417ffa1254cd4459ba98041
maiko-shimogami-fsi <133614445+maiko-shimogami-fsi@users.noreply.github.com>
2023-09-27 17:24:35 +0900

    Create check.sh

---
+++ tools/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#引数をENV_VAR_EXEに設定
+ENV_VAR_EXE=$1
+
+if [ $ENV_VAR_EXE = "env_JQ" ]; then
+ echo "env_JQ"
+elif [ $ENV_VAR_EXE = "env_WbyT" ]; then
+ echo "env_WbyT"
+else
+ echo "else"
+fi
+
+echo "end"
