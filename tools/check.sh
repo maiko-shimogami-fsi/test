6c1b26b95e317116047f78b0ca784e3380912377
maiko-shimogami-fsi <133614445+maiko-shimogami-fsi@users.noreply.github.com>
2023-09-29 09:42:53 +0900

    Update check.sh

--- tools/check.sh
+++ tools/check.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 #引数をENV_VAR_EXEに設定
-ENV_VAR_EXE=$1
-echo "ENV_VAR_EXE=$ENV_VAR_EXE"
+ENV_VAR_EXE1node=$1
+echo "ENV_VAR_EXE=$ENV_VAR_EXE1node"
 
-if [ "$ENV_VAR_EXE" = "env_JQ" ]; then
+if [ "$ENV_VAR_EXE1node" = 1 ]; then
  echo "env_JQ"
-elif [ "$ENV_VAR_EXE" = "env_WbyT" ]; then
- echo "env_WbyT"
 else
- echo "else"
+ echo "env_WbyT"
 fi
 
 echo "end"
