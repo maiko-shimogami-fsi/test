e22fbf90ad0f4d01f22a2634373160b3378d2202
maiko-shimogami-fsi <133614445+maiko-shimogami-fsi@users.noreply.github.com>
2023-11-17 19:44:32 +0900

    Update check.sh

--- tools/check.sh
+++ tools/check.sh
@@ -2,6 +2,8 @@
 INPUT_FOLDER=/home/runner/work/test/test/tools
 OUTPUT_FOLDER=/home/runner/work/test/test/tools/tools2
 
+pwd
+
 if [ ! "$(find "$INPUT_FOLDER" -name '*.mf4')" ] || [ ! "$(find "$OUTPUT_FOLDER" -name '*.mf4')" ]; then
       echo "mf4ファイルが格納されていません"
 else
