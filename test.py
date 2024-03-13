d5908301c9380d730b3d5a4cda9dc83ae08db61c
maiko-shimogami-fsi <133614445+maiko-shimogami-fsi@users.noreply.github.com>
2024-03-13 15:28:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,9 @@ def main():
     # 引数の取得
     components = sys.argv[1:]
     print(components)
+    print(components[0])
+    print(components[1])
+    print(components[2])
    
 if __name__ == "__main__":
     main()
