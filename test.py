6466ba107c0cf869b122f4785257fc863e799316
maiko-shimogami-fsi <133614445+maiko-shimogami-fsi@users.noreply.github.com>
2024-03-13 15:12:35 +0900

    Update and rename create_jobs.py to test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import yaml
+import ruamel.yaml
+import sys
+import os
+
+def main():
+    # 引数の取得
+    components = sys.argv[1:]
+    print(components)
+   
+if __name__ == "__main__":
+    main()
